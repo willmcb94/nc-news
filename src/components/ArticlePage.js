34214940266b6a94fb2842a32a176c4207f20c25
willmcb94 <williammcburney@Williams-Air.fritz.box>
2022-03-09 12:32:48 +0000

    refactored and got rid of vote state

--- src/components/ArticlePage.js
+++ src/components/ArticlePage.js
@@ -4,6 +4,7 @@ import { fetchArticleById, patchArticleVote } from "../api"
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import ThumbDownIcon from '@mui/icons-material/ThumbDown';
 import IconButton from '@mui/material/IconButton';
+import CommentIcon from '@mui/icons-material/Comment';
 
 
 
@@ -12,25 +13,27 @@ const ArticlePage = () => {
 
     const [article, setArticle] = useState({})
     const { article_id } = useParams()
-    const [votes, setVotes] = useState(0)
+    // const [votes, setVotes] = useState(0)
     const [voteChange, setVoteChange] = useState(0);
 
 
     useEffect(() => {
         fetchArticleById(article_id).then((article) => {
 
+            article.date = article.created_at.slice(3, 21)
             setArticle(article)
-            setVotes(article.votes)
+
 
         })
     }, [article_id])
 
     const handleVote = async (votecrement) => {
+
+        setVoteChange(voteChange + votecrement)
         try {
-            setVotes(votes + votecrement)
-            setVoteChange(voteChange + votecrement)
             await patchArticleVote(article.article_id, { inc_votes: votecrement })
         } catch (err) {
+
             alert("Vote unsuccesful please try again")
         }
     }
@@ -40,24 +43,24 @@ const ArticlePage = () => {
         <section className="article-main-section">
             <h2>{article.title}</h2>
             <article className="article-body-info">
+                <p className="article-body">{article.body}</p>
                 <span className="article-body-vote">
-                    <p className="article-body">{article.body}</p>
                     <span className="vote-span">
                         <IconButton color="success" onClick={() => { handleVote(1) }} disabled={voteChange === 1}><ThumbUpIcon className="vote" /></IconButton>
-                        <dt>{votes}</dt>
+                        <dt>{article.votes + voteChange}</dt>
                         <IconButton color="error" onClick={() => { handleVote(-1) }} disabled={voteChange === -1} ><ThumbDownIcon className="vote" /></IconButton>
                     </span>
+                    <span className="article-info">
+                        <dt>{`Author: ${article.author}`}</dt>
+                        <dt>{`Topic: ${article.topic}`}</dt>
+                        <dt>{article.date}</dt>
+                        <IconButton edge=""><CommentIcon className="view-comment" /> </IconButton>
+                    </span>
                 </span>
-                <span className="article-info">
-                    <h4>{article.author}</h4>
-                    <dl>
-                        <dt>{article.topic}</dt>
 
-                        <dt>{article.comments}</dt>
-
-                    </dl>
-                </span>
             </article>
+
+
         </section>
     )
 }
