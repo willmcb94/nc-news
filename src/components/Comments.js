f46e0c6a7b3502e176c4152d6a6633c1971c0037
willmcb94 <williammcburney@Williams-Air.fritz.box>
2022-03-10 09:56:36 +0000

    made a start in this branch bymistake on next task - only styling though

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -15,7 +15,9 @@ const Comments = (id) => {
 
     return (
         <section className="comments-holder">
-
+            <section className="comment-post"><h3>Post a comment</h3>
+                <input className="comment-input"></input>
+            </section>
             {comments.map((comment, index) => {
                 return <CommentCard key={`Comment-${index}`} author={comment.author} date={comment.created_at} body={comment.body} />
             })}
