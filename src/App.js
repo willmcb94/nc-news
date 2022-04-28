672ce4a2071ed0909b675d61e729861144283bb3
willmcb94 <williammcburney@Williams-Air.fritz.box>
2022-04-28 13:19:20 +0100

    add netlify.toml

--- src/App.js
+++ src/App.js
@@ -4,13 +4,8 @@ import Header from './components/Header';
 import Main from './components/Main';
 import { UserContext } from './contexts.js/UserContext';
 
-
-
-
-
-
 function App() {
-  const [user, setUser] = useState('')
+  const [user, setUser] = useState('');
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
